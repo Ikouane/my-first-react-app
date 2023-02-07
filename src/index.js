eedac951416dbfb265030425d0205ace19e2d05f
ikouane <ikouane5082@gmail.com>
2023-02-07 17:52:52 +0800

    finish all chanllenge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: ikouane
  * @Date: 2023-02-06 14:24:31
- * @LastEditTime: 2023-02-06 16:13:26
+ * @LastEditTime: 2023-02-07 17:51:46
  * @LastEditors: ikouane
  * @Description: 
  * @version: 
@@ -13,7 +13,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className='square' onClick={props.onClick}>
+    <button className={(props.className ? 'winner' : '') + ' square'} onClick={props.onClick}>
       {props.value}
     </button>
   )
@@ -21,30 +21,26 @@ function Square(props) {
 class Board extends React.Component {
   renderSquare(i) {
     return <Square
+      key={i}
       value={this.props.squares[i]}
+      className={this.props.winnerIndexArray?.includes(i)}
       onClick={() => { this.props.onClick(i) }}
     />;
   }
 
   render() {
-
+    const rowCount = [0, 1, 2], columnCount = [0, 1, 2];
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rowCount.map((r, rowIndex) =>
+          <div key={r} className="board-row">
+            {
+              columnCount.map((c, columnIndex) =>
+                this.renderSquare(3 * rowIndex + columnIndex)
+              )
+            }
+          </div>
+        )}
       </div>
     );
   }
@@ -58,7 +54,8 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
       }],
       stepNumber: 0,
-      xIsNext: true
+      xIsNext: true,
+      historyPositiveOrder: true
     }
   }
 
@@ -72,10 +69,17 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{
-        squares
+        squares,
+        location: i + 1
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    })
+  }
+
+  switchHistoryShowOrder() {
+    this.setState({
+      historyPositiveOrder: !this.state.historyPositiveOrder
     })
   }
 
@@ -86,18 +90,28 @@ class Game extends React.Component {
     })
   }
 
+  getActiveIndex(move) {
+    return (this.state.stepNumber === (this.state.historyPositiveOrder ? move : this.state.history.length - move - 1) ? 'active' : '')
+  }
+
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winner = calculateWinner(current.squares)?.winner;
+    const indexArray = calculateWinner(current.squares)?.index;
 
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
+    const historyOrder = this.state.historyPositiveOrder ? history : history.concat().reverse();
+
+    const moves = historyOrder.map((step, move) => {
+      const desc = step.location ?
+        'Go to move #' + move + `(${Math.ceil(step.location / 3)},${(step.location % 3 === 0) ? 3 : (step.location % 3)})` :
         'Go to game start';
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button
+            onClick={() => this.jumpTo(move)}
+            className={this.getActiveIndex(move)}
+          >{desc}</button>
         </li >
       )
     })
@@ -105,7 +119,10 @@ class Game extends React.Component {
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
-    } else {
+    } else if (this.state.stepNumber === 9) {
+      status = 'Draw! Everyone wins!';
+    }
+    else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
     }
 
@@ -115,10 +132,15 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            winnerIndexArray={indexArray}
           />
         </div>
         <div className="game-info">
           <div>{status}</div>
+          <button
+            className={this.state.historyPositiveOrder ? '' : 'reverse'}
+            onClick={() => this.switchHistoryShowOrder()}
+          >↓</button>
           <ol>{moves}</ol>
         </div>
       </div>
@@ -145,8 +167,21 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        winner: squares[a],
+        index: [a, b, c]
+      };
     }
   }
   return null;
 }
+
+// https://zh-hans.reactjs.org/tutorial/tutorial.html
+// 如果你还有充裕的时间，或者想练习一下刚刚学会的 React 新技能，这里有一些可以改进游戏的想法供你参考，这些功能的实现顺序的难度是递增的：
+
+// 在游戏历史记录列表显示每一步棋的坐标，格式为(列号, 行号)。
+// 在历史记录列表中加粗显示当前选择的项目。
+// 使用两个循环来渲染出棋盘的格子，而不是在代码里写死（hardcode）。
+// 添加一个可以升序或降序显示历史记录的按钮。
+// 每当有人获胜时，高亮显示连成一线的 3 颗棋子。
+// 当无人获胜时，显示一个平局的消息。
